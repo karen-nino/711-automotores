807de5e1a08a73012e5ede3c545792e7e8f6a3c9
karen-nino <46912342+karen-nino@users.noreply.github.com>
2023-08-29 14:22:35 -0600

    topnav buttons

--- app/dashboard/topnav.js
+++ app/dashboard/topnav.js
@@ -6,15 +6,22 @@ export const Topnav = () => {
             <div className='min-w-[230px] self-center bg-green-300'>
                 <div className='bg-blue-700 w-28'>Logo</div>         
             </div>
-            <div className='flex items-center justify-end px-5 '>
-                <ul>
+            <div className='flex items-center justify-end px-5 gap-[20px]'>
+                <ul className='flex gap-[20px]'>
                     <li>
-                        <a className='block px-4 py-2 bg-sky-600 '>
+                        <a className='px-4 py-2 bg-sky-600 flex items-center gap-[20px]'>
                             <div className='bg-blue-300 h-7 w-7'></div>
+                            <span>Matriz</span>
+                            <div className='w-2 h-2 bg-blue-300'></div>
                         </a>
                     </li>
-                </ul>
-                <ul>
+                    <li>
+                        <a className='px-4 py-2 bg-sky-600 flex items-center gap-[20px]'>
+                            <div className='bg-blue-300 h-7 w-7'></div>
+                            <span>Nombre completo</span>
+                            <div className='w-2 h-2 bg-blue-300'></div>
+                        </a>
+                    </li>
                     <li>
                         <a className='block px-4 py-2 bg-sky-600 '>
                             <div className='bg-blue-300 h-7 w-7'></div>
