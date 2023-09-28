bf3e8b1d65dacf843c22e8562ea66736912fa297
karen-nino <46912342+karen-nino@users.noreply.github.com>
2023-09-28 12:20:34 -0600

    page_recuperar-contraseña

---
+++ app/pages/recuperar-contrasena/page.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Image from 'next/image'
+
+const RecuperarContrasena = () => {
+  return (
+    <div>
+
+      <div className='flex justify-between py-5 bg-sky-700'>
+        <div className='w-[230px] flex justify-center items-center'>
+          <div className='flex items-center justify-center'>
+            <Image src="/assets/711-logo.png" width={200} height={100} />
+          </div>
+        </div>
+      </div>
+
+      <section className='flex flex-col items-center mt-20'>
+
+        <div className='flex flex-col'>
+
+        <p className='self-start mb-3 text-xl font-light text-gray-500'>Olvidé mi contraseña</p>
+        
+          <div className='mb-5 w-96'>
+            <input type="text" placeholder="Correo electrónico" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+          </div>
+
+
+          <a className='self-end block px-5 py-3 text-xs tracking-wide text-white bg-green-500 border border-b border-gray-300 rounded cursor-pointer item text-gray active:bg-green-700'>
+            <div className='flex justify-center'>
+              <span className='text-center'>Enviar correo de recuperación</span>
+            </div>
+          </a>
+
+          <div className='flex flex-col w-full mt-8'>
+            <a href='#' className='mb-2 text-sm text-blue-600 cursor-pointer active:text-blue-800'>Inicia sesión</a>
+            <p className='text-sm'>Si aún necesitas ayuda <span><a href='#' className='mb-2 text-blue-600 cursor-pointer active:text-blue-800'>contáctanos</a></span></p>
+          </div>
+
+        </div>
+
+      </section>
+
+    </div>
+  )
+}
+
+export default RecuperarContrasena
