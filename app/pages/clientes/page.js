1525bfbf366f80341619b02dec6280667a0dd358
karen-nino <46912342+karen-nino@users.noreply.github.com>
2023-10-02 19:58:50 -0600

    grid-clientes

--- app/pages/clientes/page.js
+++ app/pages/clientes/page.js
@@ -6,38 +6,7 @@ const Clientes = () => {
   return (
     <div className='flex flex-col'>
 
-      {/* <section className='bg-blue-400'>
-        <Topnav />
-      </section>
-      <section className='grid min-h-screen grid-cols-[230px,1fr,1fr,1fr,1fr,1fr]'>
-        <section className='col-span-1 bg-green-400'>
-          <Sidebar />
-        </section>
-        <section className='col-span-5'>
-          <div className='min-h-screen bg-gray-200'>
-            <div className='flex items-center pl-6 text-base text-white bg-gray-700 h-14'>
-              <div className='mr-3'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="14" viewBox="0 0 12 14" fill="none">
-                  <g clip-path="url(#clip0_43_969)">
-                    <path d="M11.6667 11.5846C11.6667 12.247 11.4768 12.8151 11.097 13.2891C10.7172 13.763 10.26 14 9.72526 14H1.94141C1.40668 14 0.949436 13.763 0.569661 13.2891C0.189887 12.8151 0 12.247 0 11.5846C0 11.0681 0.0258247 10.5805 0.077474 10.1217C0.129123 9.66298 0.224826 9.20117 0.364583 8.73633C0.50434 8.27148 0.682075 7.87348 0.897786 7.54232C1.1135 7.21115 1.39909 6.94075 1.75456 6.73112C2.11003 6.52148 2.51866 6.41667 2.98047 6.41667C3.77648 7.19444 4.72743 7.58333 5.83333 7.58333C6.93924 7.58333 7.89019 7.19444 8.6862 6.41667C9.148 6.41667 9.55664 6.52148 9.91211 6.73112C10.2676 6.94075 10.5532 7.21115 10.7689 7.54232C10.9846 7.87348 11.1623 8.27148 11.3021 8.73633C11.4418 9.20117 11.5375 9.66298 11.5892 10.1217C11.6408 10.5805 11.6667 11.0681 11.6667 11.5846ZM9.33333 3.5C9.33333 4.46615 8.99154 5.29102 8.30794 5.97461C7.62435 6.6582 6.79948 7 5.83333 7C4.86719 7 4.04232 6.6582 3.35872 5.97461C2.67513 5.29102 2.33333 4.46615 2.33333 3.5C2.33333 2.53385 2.67513 1.70898 3.35872 1.02539C4.04232 0.341797 4.86719 0 5.83333 0C6.79948 0 7.62435 0.341797 8.30794 1.02539C8.99154 1.70898 9.33333 2.53385 9.33333 3.5Z" fill="white" />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_43_969">
-                      <rect width="11.6667" height="14" fill="white" />
-                    </clipPath>
-                  </defs>
-                </svg>
-              </div>
-              <span>Clientes</span>
-            </div>
-          </div>
-        </section>
-      </section> */}
-
-
-
-
-      <div className="grid h-screen grid-cols-[230px,1fr,1fr,1fr,1fr,1fr] grid-rows-[1fr,1fr]">
+      <div className="grid h-screen grid-cols-[230px,1fr,1fr,1fr,1fr,1fr] grid-rows-[1fr,1fr] overflow-hidden">
 
         <section className="col-span-6">
           <Topbar />
@@ -66,7 +35,15 @@ const Clientes = () => {
               <span>Clientes</span>
             </article>
 
-            <section className='grid w-4/5 grid-cols-4 gap-3 p-5'>
+            <section className='grid w-8/12 gap-3 p-5 grid-cols-fluid'>
+
+
+              {/* <div className='h-20 bg-blue-500'></div>
+              <div className='h-20 bg-blue-500'></div>
+              <div className='h-20 bg-blue-500'></div>
+              <div className='h-20 bg-blue-500'></div>
+              <div className='h-20 bg-blue-500'></div> */}
+
 
               <article className='flex h-20 p-3 bg-white cursor-pointer'>
 
@@ -111,7 +88,7 @@ const Clientes = () => {
 
               </article>
 
-              <article className='flex h-20 p-3 bg-white'>
+              <article className='flex h-20 p-3 bg-white cursor-pointer'>
 
                 <div>
                   <div className='flex items-center justify-center bg-gray-300 rounded-full h-14 w-14'>
@@ -154,7 +131,7 @@ const Clientes = () => {
 
               </article>
 
-              <article className='flex h-20 p-3 bg-white'>
+              <article className='flex h-20 p-3 bg-white cursor-pointer'>
 
                 <div>
                   <div className='flex items-center justify-center bg-gray-300 rounded-full h-14 w-14'>
@@ -184,7 +161,6 @@ const Clientes = () => {
                       </div>
                       <p>7111234567</p>
                     </div>
-                    
                   </div>
 
                   <div>
@@ -198,7 +174,7 @@ const Clientes = () => {
 
               </article>
 
-              <article className='flex h-20 p-3 bg-white'>
+              <article className='flex h-20 p-3 bg-white cursor-pointer'>
 
                 <div>
                   <div className='flex items-center justify-center bg-gray-300 rounded-full h-14 w-14'>
@@ -228,7 +204,6 @@ const Clientes = () => {
                       </div>
                       <p>7111234567</p>
                     </div>
-                    
                   </div>
 
                   <div>
@@ -242,7 +217,7 @@ const Clientes = () => {
 
               </article>
 
-              <article className='flex h-20 p-3 bg-white'>
+              <article className='flex h-20 p-3 bg-white cursor-pointer'>
 
                 <div>
                   <div className='flex items-center justify-center bg-gray-300 rounded-full h-14 w-14'>
@@ -272,7 +247,6 @@ const Clientes = () => {
                       </div>
                       <p>7111234567</p>
                     </div>
-                    
                   </div>
 
                   <div>
@@ -286,7 +260,7 @@ const Clientes = () => {
 
               </article>
 
-              <article className='flex h-20 p-3 bg-white'>
+              <article className='flex h-20 p-3 bg-white cursor-pointer'>
 
                 <div>
                   <div className='flex items-center justify-center bg-gray-300 rounded-full h-14 w-14'>
@@ -316,7 +290,6 @@ const Clientes = () => {
                       </div>
                       <p>7111234567</p>
                     </div>
-                    
                   </div>
 
                   <div>
@@ -330,7 +303,7 @@ const Clientes = () => {
 
               </article>
 
-              <article className='flex h-20 p-3 bg-white'>
+              <article className='flex h-20 p-3 bg-white cursor-pointer'>
 
                 <div>
                   <div className='flex items-center justify-center bg-gray-300 rounded-full h-14 w-14'>
@@ -360,7 +333,6 @@ const Clientes = () => {
                       </div>
                       <p>7111234567</p>
                     </div>
-                    
                   </div>
 
                   <div>
@@ -373,24 +345,6 @@ const Clientes = () => {
                 </div>
 
               </article>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
             </section>
