f44df6b55d6054514a4cd4cd02eccd4a827a5f07
karen-nino <46912342+karen-nino@users.noreply.github.com>
2023-11-03 18:43:19 -0600

    detalles de prenda - historial

--- app/pages/historial-prendas/detalles-de-prendas/page.js
+++ app/pages/historial-prendas/detalles-de-prendas/page.js
@@ -21,7 +21,7 @@ const DetallesDePrendas = () => {
                         <div className='h-screen bg-gray-200'>
 
                             <article className='flex items-center pl-6 text-base text-white bg-gray-700 h-14'>
-                                <Link href="../historial-prendas" className='mr-3'>
+                                <Link href="../prendas" className='mr-3'>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className='cursor-pointer fill-white hover:fill-gray-400'>
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M15 20L7 12L15 4L17 6L11 12L17 18L15 20Z" />
                                     </svg>
@@ -35,19 +35,72 @@ const DetallesDePrendas = () => {
 
                                 <article className='pb-10'>
                                     <div className='w-full p-4 text-white bg-gray-700'>
-                                        <h3>Ford Focus 2009</h3>
+                                        <h3>Nissan Focus 2013</h3>
                                     </div>
 
                                     <div className='w-full px-4 py-6 text-gray-500 break-words bg-white'>
                                         <div className='flex pb-3 text-sm'>
                                             <div className='w-[150px]'>Stock</div>
-                                            <div className=''>9-1</div>
+                                            <div className=''>1-1</div>
                                         </div>
                                         <div className='flex pb-3 text-sm'>
                                             <div className='w-[150px]'>Dueño original</div>
-                                            <div className=''>Héctor Hernández Copca</div>
+                                            <div className=''>Emiliano Gomez Barrones</div>
+                                        </div>
+                                        <div className='flex pb-3 text-sm'>
+                                            <div className='w-[150px]'>Descripción</div>
+                                            <div className=''>Nissan Focus 2023</div>
+                                        </div>
+                                        <div className='flex pb-3 text-sm'>
+                                            <div className='w-[150px]'>Detalles</div>
+                                            <div className=''>No. Serie 300 2000km</div>
+                                        </div>
+                                        <div className='flex pb-3 text-sm'>
+                                            <div className='w-[150px]'>Observaciones</div>
+                                            <div className=''>Perfectas condiciones</div>
+                                        </div>
+                                        <div className='flex pb-3 text-sm'>
+                                            <div className='w-[150px]'>Avalúo</div>
+                                            <div className=''>$2.00</div>
+                                        </div>
+                                        <div className='flex pb-3 text-sm'>
+                                            <div className='w-[150px]'>Fecha venta</div>
+                                            <div className=''>22/06/2023</div>
+                                        </div>
+                                        <div className='flex pb-3 text-sm'>
+                                            <div className='w-[150px]'>Prestado</div>
+                                            <div className=''>$200,000.00</div>
+                                        </div>
+                                        <div className='flex pb-3 text-sm'>
+                                            <div className='w-[150px]'>Precio venta</div>
+                                            <div className=''>$210,000.00</div>
+                                        </div>
+                                        <div className='flex pb-3 text-sm'>
+                                            <div className='w-[150px]'>Adquisición</div>
+                                            <div className=''>Empeño</div>
+                                        </div>
+                                        <div className='flex pb-3 text-sm'>
+                                            <div className='w-[150px]'>Estado</div>
+                                            <div className=''>Vendido</div>
+                                        </div>
+                                        <div className='flex pb-3 text-sm'>
+                                            <div className='w-[150px]'>Vendido a</div>
+                                            <div className=''>Lizbeth Ramos</div>
+                                        </div>
+                                        <div className='flex pb-3 text-sm'>
+                                            <div className='w-[150px]'>Vendido por</div>
+                                            <div className=''>Orlando Daniel Virgen Z</div>
                                         </div>
                                     </div>
+                                </article>
+                                
+                                <article className='pb-10'>
+                                    <div className='w-full p-4 text-white bg-gray-700'>
+                                        <h3>Fotos</h3>
+                                    </div>
+
+                                    {/* <div className='w-full px-4 py-6 text-gray-500 break-words bg-white'>
+                                    </div> */}
                                 </article>
 
                             </section>
