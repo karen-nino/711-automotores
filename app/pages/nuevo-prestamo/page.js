e055f370dffdfa8e6aa55bcf788dec5c64aefb0a
karen-nino <46912342+karen-nino@users.noreply.github.com>
2023-11-06 15:52:30 -0600

    version

--- app/pages/nuevo-prestamo/page.js
+++ app/pages/nuevo-prestamo/page.js
@@ -77,6 +77,11 @@ const NuevoPrestamo = () => {
                                     </div>
 
                                     <div className='mb-5'>
+                                        <label for="modelo" class="block mb-2 text-sm font-semibold text-gray-700">Versión</label>
+                                        <input type="text" placeholder="Modelo" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                                    </div>
+
+                                    <div className='mb-5'>
                                         <label for="serie" class="block mb-2 text-sm font-semibold text-gray-700">Serie</label>
                                         <input type="text" placeholder="Serie" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
                                     </div>
