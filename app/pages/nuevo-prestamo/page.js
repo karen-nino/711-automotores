9914cb3590c265c45b9f181f880a2783df9baccf
karen-nino <46912342+karen-nino@users.noreply.github.com>
2023-11-16 13:05:21 -0600

    submarca motos

--- app/pages/nuevo-prestamo/page.js
+++ app/pages/nuevo-prestamo/page.js
@@ -101,6 +101,11 @@ const NuevoPrestamo = () => {
                                         </div>
                                     </div>
 
+                                    <div className='mb-5'>
+                                        <label for="linea" class="block mb-2 text-sm font-semibold text-gray-700">Submarca - Motos</label>
+                                        <input type="text" placeholder="Línea" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                                    </div>
+
 
                                     <div className='mb-5'>
                                         <label for="linea" class="block mb-2 text-sm font-semibold text-gray-700">Línea</label>
